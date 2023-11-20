dfef56f0601db9d394c73dbe13a09dc858441e2d
Ishak Amri <amri.isk@gmail.com>
2023-11-20 17:40:21 +0100

    -Redirect to welcome from validator

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/validator/login', function () {
+    return redirect('/');
+})->name("filament.validator.auth.login");
